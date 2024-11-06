d931c53b94467de067d91afb5aedff54ae659108
Marcello Comandulli <marcello.coma@gmail.com>
2024-11-06 17:53:57 +0100

    added React Helmet

--- src/components/AroundMe.jsx
+++ src/components/AroundMe.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Helmet } from "react-helmet";
 
 export default function AroundMe() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Around Me - ItalyCharge</title>
+      </Helmet>
+
       <iframe
         src="https://map.openchargemap.io/?mode=embedded"
         allow="geolocation"
